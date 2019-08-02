a4ac2052b9fed6abefca6410c9f2db0336145480
tavisca-vraut <vraut@tavisca.in>
2019-08-02 16:59:17 +0530

    Add full implementation for ResponseBuilder

--- SocketResponse/ResponseBuilder.cs
+++ SocketResponse/ResponseBuilder.cs
@@ -1,16 +1,106 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace SocketResponse
 {
     public class ResponseBuilder
     {
+        private byte[] Header { get; set; }
+        public StringBuilder ResponseMessage { get; private set; }
         public const string HttpVersion = "HTTP/1.1";
         public const string Server = "Localhost (RV): Windows 10 64-bit";
-        public object Date { get; private set; }
+        public DateTime? Date { get; set; } = null;
+        public int? ContentLength { get; set; } = null;
+        public string ContentType { get; set; } = null;
+        public int StatusCode { get; set; } = 200;
+        public string StatusMessage { get; set; } = "OK";
+        public byte[] Data { get; set; } = null;
 
-        public void SetDate(DateTime dateTime)
+        public byte[] GetResponseAsBytes()
         {
-            this.Date = dateTime;
+            BuildResponseHeaderString();
+            ConvertHeaderToBytes();
+            return GenerateResponseBytes();
+        }
+
+        private byte[] GenerateResponseBytes()
+        {
+            List<byte> response = new List<byte>();
+
+            response.AddRange(Header);
+            response.AddRange(Data);
+
+            return response.ToArray();
+        }
+
+        private void ConvertHeaderToBytes()
+        {
+            Header = Encoding.ASCII.GetBytes(ResponseMessage.ToString());
+        }
+
+        public void BuildResponseHeaderString()
+        {
+            ResponseMessage = new StringBuilder();
+            AddStatusLineToResponse();
+
+            if (IsSetDate(Date))
+                AddDateToResponseMessage();
+
+            AddServerToResponse();
+
+            if (IsSetContentLength(ContentLength))
+                AddContentLengthToResponseMessage();
+
+            if (IsSetContentType(ContentType))
+                AddContentTypeToResponseMessage();
+
+            ResponseMessage.AppendLine();
+        }
+
+        private void AddContentTypeToResponseMessage()
+        {
+            ResponseMessage.AppendLine($"Content-Type: {ContentType}");
+        }
+
+        private void AddContentLengthToResponseMessage()
+        {
+            ResponseMessage.AppendLine($"Content-Length: {ContentLength}");
+        }
+
+        private void AddServerToResponse() => ResponseMessage.AppendLine($"Server: {Server}");
+
+        private void AddStatusLineToResponse()
+        {
+            ResponseMessage.AppendLine($"{HttpVersion} {StatusCode} {StatusMessage}");
+        }
+
+        private void AddDateToResponseMessage()
+        {
+            ResponseMessage.AppendLine($"Date: {Date.Value.ToString("ddd, dd MMM yyyy hh:mm:ss IST")}");
+        }
+
+        private bool IsSetContentLength(int? contentLength)
+        {
+            if (contentLength == null)
+                throw new ArgumentNullException("Content Length is NULL.");
+            return true;
+        }
+        private bool IsSetContentType(string contentType)
+        {
+            if (ContentType == null)
+                throw new ArgumentNullException("Content Type is NULL.");
+            return true;
+        }
+        private bool IsSetData(byte[] data)
+        {
+            if (data == null)
+                throw new ArgumentNullException("Data is NULL.");
+            return true;
+        }
+        public bool IsSetDate(DateTime? date)
+        {
+            return date != null;
         }
     }
 }
--- WebServer.Tests/ResponseBuilderFixture.cs
+++ WebServer.Tests/ResponseBuilderFixture.cs
@@ -2,6 +2,8 @@ using System;
 using Xunit;
 using FluentAssertions;
 using SocketResponse;
+using System.Text;
+using System.Collections.Generic;
 
 namespace WebServer.Tests
 {
@@ -26,8 +28,127 @@ namespace WebServer.Tests
 
             var currentDateTime = DateTime.Now;
 
-            response.SetDate(currentDateTime);
+            response.Date = currentDateTime;
             response.Date.Should().Be(currentDateTime);
+        }
+
+        [Fact]
+        public void Response_set_content_length_test()
+        {
+            var response = new ResponseBuilder
+            {
+                ContentLength = 100
+            };
+
+            response.ContentLength.Should().Be(100);
+        }
+
+        [Fact]
+        public void Response_set_content_type_test()
+        {
+            var response = new ResponseBuilder
+            {
+                ContentType = "text/html"
+            };
+
+            response.ContentType.Should().Be("text/html");
+        }
+
+        [Fact]
+        public void Response_set_status_code_test()
+        {
+            var response = new ResponseBuilder
+            {
+                StatusCode = 200
+            };
+            response.StatusCode.Should().Be(200);
+        }
+
+        [Fact]
+        public void Response_set_status_message_test()
+        {
+            var response = new ResponseBuilder
+            {
+                StatusMessage = "OK"
+            };
+            response.StatusMessage.Should().Be("OK");
+        }
+
+        [Fact]
+        public void Response_set_data_test()
+        {
+            var response = new ResponseBuilder
+            {
+                Data = Encoding.ASCII.GetBytes("OK")
+            };
+            response.Data.Should().BeEquivalentTo(Encoding.ASCII.GetBytes("OK"));
+        }
+
+        [Fact]
+        public void Response_header_builder_test()
+        {
+            int statusCode = 200;
+            string statusMessage = "OK";
+            string data = "<h1>Hello</h1>";
+            string contentType = "text/html";
+            int contentLength = data.Length;
+            var dateTime = DateTime.Now;
+
+            var expectedResponseBuilder = new StringBuilder();
+            expectedResponseBuilder.AppendLine($"{ResponseBuilder.HttpVersion} {statusCode} {statusMessage}");
+            expectedResponseBuilder.AppendLine($"Date: {dateTime.ToString("ddd, dd MMM yyyy hh:mm:ss IST")}");
+            expectedResponseBuilder.AppendLine($"Server: {ResponseBuilder.Server}");
+            expectedResponseBuilder.AppendLine($"Content-Length: {contentLength}");
+            expectedResponseBuilder.AppendLine($"Content-Type: {contentType}");
+            expectedResponseBuilder.AppendLine();
+
+            var responseObject = new ResponseBuilder
+            {
+                StatusCode = 200,
+                StatusMessage = "OK",
+                Data = Encoding.ASCII.GetBytes(data),
+                ContentType = contentType,
+                ContentLength = contentLength,
+                Date = dateTime
+            };
+
+            responseObject.BuildResponseHeaderString();
+            responseObject.ResponseMessage.Should().BeEquivalentTo(expectedResponseBuilder);
+        }
+
+        [Fact]
+        public void Response_message_in_bytes_test()
+        {
+            int statusCode = 200;
+            string statusMessage = "OK";
+            string data = "<h1>Hello</h1>";
+            string contentType = "text/html";
+            int contentLength = data.Length;
+            var dateTime = DateTime.Now;
+
+            var expectedResponseBuilder = new StringBuilder();
+            expectedResponseBuilder.AppendLine($"{ResponseBuilder.HttpVersion} {statusCode} {statusMessage}");
+            expectedResponseBuilder.AppendLine($"Date: {dateTime.ToString("ddd, dd MMM yyyy hh:mm:ss IST")}");
+            expectedResponseBuilder.AppendLine($"Server: {ResponseBuilder.Server}");
+            expectedResponseBuilder.AppendLine($"Content-Length: {contentLength}");
+            expectedResponseBuilder.AppendLine($"Content-Type: {contentType}");
+            expectedResponseBuilder.AppendLine();
+
+            var exptedResponse = new List<byte>();
+            exptedResponse.AddRange(Encoding.ASCII.GetBytes(expectedResponseBuilder.ToString()));
+            exptedResponse.AddRange(Encoding.ASCII.GetBytes(data));
+
+            var responseObject = new ResponseBuilder
+            {
+                StatusCode = 200,
+                StatusMessage = "OK",
+                Data = Encoding.ASCII.GetBytes(data),
+                ContentType = contentType,
+                ContentLength = contentLength,
+                Date = dateTime
+            };
+
+            responseObject.GetResponseAsBytes().ToString().Should().BeEquivalentTo(exptedResponse.ToArray().ToString());
         }
     }
 }
